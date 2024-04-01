e9882eb61de4e8a8cbc682fe43e1898ce88fbc69
AdelinaMakarevich <110402319+AdelinaMakarevich@users.noreply.github.com>
2024-04-01 23:46:40 +0300

    fix problem

---
+++ App.js
@@ -0,0 +1,27 @@
+import AboutMeCard from './src/Components/AboutMeCard';
+import BeforeAfterCard from './src/Components/BeforeAfterCard';
+import ContactsCard from './src/Components/ContactsCard';
+import CourseProgramCard from './src/Components/CourseProgramCard';
+import Header from './src/Components/Header'
+import OpportunityCard from './src/Components/OpportunityCard';
+import PriceCard from './src/Components/PriceCard';
+import TitleCard from './src/Components/TitleCard';
+import TriggerCard from './src/Components/TriggerCard';
+
+function App() {
+  return (
+    <>
+    <Header />
+    <TitleCard />
+    <OpportunityCard />
+    <BeforeAfterCard />
+    <CourseProgramCard />
+    <AboutMeCard />
+    <TriggerCard />
+    <PriceCard />
+    <ContactsCard />
+    </>
+  );
+}
+
+export default App;
